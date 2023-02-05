42b62aa05e09e70d644990b77598a191c8e5dc83
Alexandr Rybalko <alexmobilepost@gmail.com>
2023-02-05 21:05:49 +0300

    initial commit

---
+++ src/MapControl.tsx
@@ -0,0 +1,15 @@
+import { useMapEvents } from "react-leaflet";
+
+function MapControl() {
+//   const map = useMapEvents({
+//     click: (e) => {
+//       alert(e.latlng.);
+//     },
+//     locationfound: (location) => {
+//       console.log("location found:", location);
+//     },
+//   });
+//   return null;
+}
+
+export default MapControl;
