2629a095bae811582ee8638e2cf322d9c39e6fab
Alexandr Rybalko <alexmobilepost@gmail.com>
2023-02-07 00:01:05 +0300

    markers work

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { LeafletMouseEvent } from "leaflet";
 import { useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import MapControl from "./MapControl";
+import Routing from "./Routing";
 
 export interface LatLng {
   lat: number;
@@ -9,15 +11,40 @@ export interface LatLng {
 
 function App() {
   const [points, setPoints] = useState<LatLng[]>([]);
-  alert(points.length);
+
+  const onClickMarker = (e: LeafletMouseEvent) => {
+    const targetMarkerIndex = points.findIndex(
+      (val) => val.lat === e.latlng.lat && val.lng === e.latlng.lng
+    );
+    setPoints((prevState) => {
+      const newState = [...prevState];
+      newState.splice(targetMarkerIndex, 1);
+      return newState;
+    });
+  };
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer id="map" center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <MapControl setPoints={setPoints} />
+      {points.map((val) => (
+        <Marker
+          key={`${val.lat}${val.lng}`}
+          eventHandlers={{
+            click: (e) => onClickMarker(e),
+            mouseover: (e) => console.log(e.sourceTarget),
+          }}
+          position={val}
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      ))}
+      <Routing />
     </MapContainer>
   );
 }
--- src/MapControl.tsx
+++ src/MapControl.tsx
@@ -2,6 +2,7 @@ import { LeafletMouseEvent } from "leaflet";
 import { Dispatch, FC, SetStateAction } from "react";
 import { useMapEvents } from "react-leaflet";
 import { LatLng } from "./App";
+import L from "leaflet";
 
 interface Props {
   setPoints: Dispatch<SetStateAction<LatLng[]>>;
@@ -9,15 +10,19 @@ interface Props {
 
 const MapControl: FC<Props> = ({ setPoints }) => {
   const onClickMap = (e: LeafletMouseEvent) => {
-    setPoints((prevState) => [
-      ...prevState,
-      { lat: e.latlng.lat, lng: e.latlng.lng },
-    ]);
+    setPoints((prevState) => {
+      const isMarkerExist =
+        prevState.findIndex(
+          (val) => val.lat === e.latlng.lat && val.lng === e.latlng.lng
+        ) !== -1;
+      if (isMarkerExist) return prevState;
+      return [...prevState, { lat: e.latlng.lat, lng: e.latlng.lng }];
+    });
   };
   useMapEvents({
     click: (e) => onClickMap(e),
   });
-
+  
   return null;
 };
 
